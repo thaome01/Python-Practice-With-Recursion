f6c40d11e3a6e73199f37cce6a1c683a6c2e1c2a
Meng Thao <mengthao023@gmail.com>
2022-11-02 20:07:32 -0500

    solution to the algorithms

--- solution1.py
+++ solution1.py
@@ -1,1 +1,33 @@
 # Write code for algorithm 1 below
+
+# Write a program that takes a positive number as an argument and prints the numbers from n to zero
+
+def count_down(n):
+
+  # Base case
+  if n==0:
+      return
+
+  # Recursive case
+  else:
+      print(n)
+      count_down(n-1)
+
+# test case
+n=8
+count_down(n)
+
+    #or
+
+def count_down(n):
+  #inherent base case
+  #(will stop if n <= 0)
+  if n>0:
+      #recursive case
+      print(n)
+      count_down(n-1)
+   
+#test case
+n=8
+count_down(n)
+
--- solution2.py
+++ solution2.py
@@ -1,1 +1,15 @@
-# Write code for algorithm 2 below
+# 2. Write a function that prints the natural numbers from 1 to n.
+
+def natural_numbers(x,i=1):
+	#base case
+  if i > x:
+    return
+  #recursive case
+  else:
+    print(i)
+    natural_numbers(x,i+1)
+natural_numbers(3)
+#output: 1 2 3
+
+    
+
